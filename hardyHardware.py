297252c01312400cc3d6088399ba5dbc5aba3dd9
Ido Avnir <62429637+21dododo@users.noreply.github.com>
2021-02-09 19:25:42 +0200

    updated HTML

--- hardyHardware.py
+++ hardyHardware.py
@@ -10,20 +10,20 @@ WEBSITE_URL = "http://class.kofax.com/hardyhardware/"
 NUM_OF_PAGES = 3
 
 #function for printing all the items
-def print_items(conn, db):
-    with conn:
-        db.execute("SELECT * FROM Items")
-        rows = db.fetchall()
-        i = 1
-        for row in rows:
-            name, stars, price, description, overeview = row
-            print(f"Item {i}- Name: {name}")
-            print(f"        Stars: {stars}")
-            print(f"        Price: ${price}")
-            print(f"        Description: {description}")
-            print(f"        Overeview: {overeview}")
+def print_items(db):
 
-            i += 1
+    db.execute("SELECT * FROM Items")
+    rows = db.fetchall()
+    i = 1
+    for row in rows:
+        name, stars, price, description, overeview = row
+        print(f"Item {i}- Name: {name}")
+        print(f"        Stars: {stars}")
+        print(f"        Price: ${price}")
+        print(f"        Description: {description}")
+        print(f"        Overeview: {overeview}")
+
+        i += 1
 def main():
     conn = sqlite3.connect('hardyDB.db')
     db = conn.cursor()
@@ -47,19 +47,19 @@ def main():
     driver.find_element(*MainLocators.PRODUCTS_LISTING_BUTTON).click()
     #wait until the page has loaded
     try:
-        exwait = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located(MainLocators.PRODUCTS_LISTING_BREADCRUMB))
+        WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(MainLocators.PRODUCTS_LISTING_BREADCRUMB))
         #loop the first 3 pages
-        for page in range(0,NUM_OF_PAGES):
-            if page != 0:
-                # go to the next page unless it's our first iteration
-                driver.find_element_by_xpath(f'//*[@id="hikashop_category_information_module_88"]/div/form/div/div/ul/li[{page + 3}]/a').click()
+        for page in range(1, NUM_OF_PAGES + 1):
+            if page != 1:
+                # go to the next page unless it's our first iteration (we do page + 2 because the button of page 1 is in li[3] and so on)
+                driver.find_element_by_xpath(f'//*[@id="hikashop_category_information_module_88"]/div/form/div/div/ul/li[{page + 2}]/a').click()
                 # check that the page has loaded
-                exwait = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, 'hikashop_products_listing')))
+                WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'hikashop_products_listing')))
             ##loop through all of the products in the page
             elements = driver.find_element(By.CLASS_NAME, "hikashop_products")
-            num_of_elements = len (elements.find_elements (By.CLASS_NAME, "hikashop_container"))
+            num_of_elements = len (elements.find_elements (*MainLocators.ALL_PRODUCTS))
             for x in range (0, num_of_elements):
                 #I have to re-initialize the elements every loop run because every time I go back to the product's page the DOM changes.
                 elements = driver.find_elements(*MainLocators.ALL_PRODUCTS)
@@ -67,8 +67,8 @@ def main():
                 #click the product's name to enter its page
                 element.find_element(*MainLocators.PRODUCT_NAME).click()
                 #wait until the page has loaded
-                exwait = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located(ProductPageLocators.MAIN_IMAGE))
+                WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(ProductPageLocators.MAIN_IMAGE))
                 #start pulling information
                 name = driver.find_element(*ProductPageLocators.NAME).text
                 stars = float (driver.find_element(*ProductPageLocators.STARS).get_attribute("value"))
@@ -88,13 +88,13 @@ def main():
                 except NoSuchElementException:
                     driver.refresh()
                 #wait until the page has loaded
-                exwait = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located(MainLocators.PRODUCTS_LISTING_BREADCRUMB))
+                WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(MainLocators.PRODUCTS_LISTING_BREADCRUMB))
 
     finally:
         driver.quit()
 
-    print_items(conn, db)
+    print_items(db)
 
     conn.commit()
     conn.close()
